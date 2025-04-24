c08b82bdbfa8537766e4d517418d1a18655b98ef
Kinjal-Patel23 <kkaneriya11@gmail.com>
2025-04-24 14:04:38 +0530

    real-estate-webpage

--- real-estate_webpage/src/Components/Process.jsx
+++ real-estate_webpage/src/Components/Process.jsx
@@ -1,43 +1,9 @@
 import React from 'react'
-// import { useState, useEffect, useRef } from 'react'
 import processImg1 from '../assets/process-thumb-1.jpg'
 import processImg2 from '../assets/process-thumb-2.jpg'
 import processImg3 from '../assets/process-thumb-3.jpg'
 
 const Process = () => {
-
-    // const [currentStep, setCurrentStep] = useState('step1');
-
-    // const step1Ref = useRef(null)
-    // const step2Ref = useRef(null)
-    // const step3Ref = useRef(null)
-
-    // useEffect(() => {
-    //     const handleScroll = () => {
-    //         const scrollY = window.scrollY + window.innerHeight / 2 // center of screen
-
-
-    //         const step2Top = step2Ref.current.offsetTop
-    //         const step3Top = step3Ref.current.offsetTop
-
-    //         if (scrollY < step2Top) {
-    //             setCurrentStep('step1')
-    //         } else if (scrollY >= step2Top && scrollY < step3Top) {
-    //             setCurrentStep('step2')
-    //         } else {
-    //             setCurrentStep('step3')
-    //         }
-    //     }
-
-    //     window.addEventListener('scroll', handleScroll)
-    //     return () => window.removeEventListener('scroll', handleScroll)
-    // }, []);
-
-    // const getImage = () => {
-    //     if (currentStep === 'step2') return processImg2
-    //     if (currentStep === 'step3') return processImg3
-    //     return processImg1
-    // }
 
     return (
         <>
@@ -57,12 +23,14 @@ const Process = () => {
                                 <div className='stepText w-[80px] h-[40px] mt-[40px] mx-auto lg:mx-0 bg-[#F7F7F7] border-2 border-[#E8E8E8] rounded-[50px] flex items-center justify-center'>
                                     <p className='text-[13px] sm:text-[14px] uppercase text-[#333]'>step-1</p>
                                 </div>
-                                <p className='text-[24px] sm:text-[32px] lg:text-[48px] font-semibold leading-snug text-center lg:text-left pt-[20px]'>
+                                <p className='text-[24px] sm:text-[32px] lg:text-[48px] font-semibold leading-snug lg:leading-[51px] text-center lg:text-left pt-[20px]'>
                                     Look for your dream home in your local area today
                                 </p>
                                 <p className='text-[14px] text-[#333] leading-[21px] pt-[18px] text-center lg:text-left'>
                                     Explore a carefully curated selection of stunning homes near you that <br />
-                                    perfectly match your unique lifestyle, preferences, and specific needs...
+                                    perfectly match your unique lifestyle, preferences, and specific needs, <br />
+                                    offering the ideal balance of comfort, style, luxury, and location. Each <br />
+                                    property is handpicked to provide the perfect combination.
                                 </p>
                                 <div className='block lg:hidden mt-4'>
                                     <img src={processImg1} className="rounded-[20px] w-full h-auto" />
@@ -72,11 +40,13 @@ const Process = () => {
                                 <div className='stepText w-[80px] h-[40px] mt-[40px] mx-auto lg:mx-0 bg-[#F7F7F7] border-2 border-[#E8E8E8] rounded-[50px] flex items-center justify-center'>
                                     <p className='text-[13px] sm:text-[14px] uppercase text-[#333]'>step-2</p>
                                 </div>
-                                <p className='text-[24px] sm:text-[32px] lg:text-[46px] font-semibold leading-snug text-center lg:text-left pt-[20px]'>
+                                <p className='text-[24px] sm:text-[32px] lg:text-[46px] font-semibold leading-snug lg:leading-[51px] text-center lg:text-left pt-[20px]'>
                                     Schedule a meeting with one of our agents
                                 </p>
                                 <p className='text-[14px] text-[#333] leading-[21px] pt-[18px] text-center lg:text-left'>
-                                    Book a personalized meeting with one of our experienced agents...
+                                    Book a personalized meeting with one of our experienced agents to <br />
+                                    thoroughly explore all your options, discuss your needs, and find the <br />
+                                    perfect property that fits your lifestyle and budget.
                                 </p>
                                 <div className='block lg:hidden mt-4'>
                                     <img src={processImg2} className="rounded-[20px] w-full h-auto" />
@@ -86,11 +56,14 @@ const Process = () => {
                                 <div className='stepText w-[80px] h-[40px] mt-[40px] mx-auto lg:mx-0 bg-[#F7F7F7] border-2 border-[#E8E8E8] rounded-[50px] flex items-center justify-center'>
                                     <p className='text-[13px] sm:text-[14px] uppercase text-[#333]'>step-3</p>
                                 </div>
-                                <p className='text-[24px] sm:text-[32px] lg:text-[48px] font-semibold leading-snug text-center lg:text-left pt-[20px]'>
+                                <p className='text-[24px] sm:text-[32px] lg:text-[48px] font-semibold leading-snug lg:leading-[51px] text-center lg:text-left pt-[20px]'>
                                     A month or less, get your ideal home
                                 </p>
                                 <p className='text-[14px] text-[#333] leading-[21px] pt-[18px] text-center lg:text-left'>
-                                    Secure your dream home in a month or less with our expert assistance...
+                                    Secure your dream home in a month or less with our expert assistance, <br />
+                                    personalized guidance, and streamlined processes, ensuring a smooth, <br />
+                                    stress-free, and successful journey tailored specifically to your needs and <br />
+                                    preferences from start to finish.
                                 </p>
                                 <div className='block lg:hidden mt-4'>
                                     <img src={processImg3} className="rounded-[20px] w-full h-auto" />
@@ -101,7 +74,6 @@ const Process = () => {
                             <img src={processImg1} className="rounded-[20px] w-full h-full object-cover" />
                         </div>
                     </div>
-
                 </div>
             </div>
         </>
